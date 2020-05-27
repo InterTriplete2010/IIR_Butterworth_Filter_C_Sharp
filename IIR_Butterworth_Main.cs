8d4d7e7330b0ea10bda864d7c9ff820019ea3c07
InterTriplete2010 <60267824+InterTriplete2010@users.noreply.github.com>
2020-05-27 09:34:17 -0400

    Update IIR_Butterworth_Main.cs
    Changes have been made to allow the user to filter the signal. Please, keep in mind that pre and post filter data along with the time domain are now saved in 3 different txt files, so the results can be imported and used in other softwares, such as Matlab. The user needs to change the directory where the txt files will be saved. That is line 39: 
    
    string dir = @"C:\Users\press\OneDrive\Desktop\IIR_Butterworth_C_Sharp";

--- IIR_Butterworth_Main.cs
+++ IIR_Butterworth_Main.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Numerics;
 using System.Collections.Generic;
 using System.Text;
@@ -8,7 +9,7 @@ using System.Linq;
 using MathNet.Numerics;
 using Combinatorics;
 using Combinatorics.Collections;
-
+using System.Runtime.InteropServices.ComTypes;
 
 namespace IIR_Butterworth_C_Sharp
 {
@@ -21,8 +22,8 @@ namespace IIR_Butterworth_C_Sharp
 
             IIR_Butterworth_Interface IBI = new IIR_Butterworth_C_Sharp.IIR_Butterworth_Implementation();
 
-            double f1 = 1;  //High Pass
-            double f2 = 30; //Low Pass
+            double f1 = 30;  //High Pass
+            double f2 = 40; //Low Pass
             double sf = 2048;    //Sampling frequency
             int order_filt = 3; //Order
             double Nyquist_F = sf / 2;
@@ -32,8 +33,65 @@ namespace IIR_Butterworth_C_Sharp
 
             int type_filt = 3;
             
-            
             int coeff_numb = 0;
+
+            //Directory where to save the pre-filt and post-filt data
+            string dir = @"C:\Users\press\OneDrive\Desktop\IIR_Butterworth_C_Sharp";
+            try
+            {
+                //Set the current directory.
+                Directory.SetCurrentDirectory(dir);
+
+            }
+
+            catch (DirectoryNotFoundException e)
+            {
+
+                Console.WriteLine("The specified directory does not exist. {0}", e);
+            
+            }
+
+            //Build the signal to be used to test the filter
+            int length_test_signal = 2047;
+            double[] test_signal = new double[length_test_signal];
+            double[] time = new double[length_test_signal];
+            double freq_data_I = 10;
+            double freq_data_II = 80;
+
+            double[] output_filt_signal = new double[length_test_signal];
+            
+            for (int kk = 0; kk < length_test_signal; kk++)
+            {
+
+                time[kk] = (double)kk / sf;
+                test_signal[kk] = Math.Sin(2 * Math.PI * time[kk] * freq_data_I) + Math.Sin(2 * Math.PI * time[kk] * freq_data_II);
+
+            }
+
+            using (StreamWriter sw = new StreamWriter("Pre_Filtered_data.txt"))
+            {
+
+                for (int hh = 0; hh < length_test_signal; hh++)
+                {
+
+                    sw.WriteLine(Convert.ToString(test_signal[hh]));
+
+                }
+
+            }
+
+            using (StreamWriter sw = new StreamWriter("Time_domain.txt"))
+            {
+
+                for (int hh = 0; hh < length_test_signal; hh++)
+                {
+
+                    sw.WriteLine(Convert.ToString(time[hh]));
+
+                }
+
+            }
+
             switch (type_filt)
             {
                 case 0:
@@ -53,8 +111,23 @@ namespace IIR_Butterworth_C_Sharp
 
                     stability_check = IBI.Check_stability_iir(coeff_final);
 
+                    output_filt_signal = IBI.Filter_Data(coeff_final, test_signal);
+
+                    using (StreamWriter sw = new StreamWriter("Filtered_data.txt"))
+                    {
+
+                        for (int hh = 0; hh < length_test_signal; hh++)
+                        {
+
+                            sw.WriteLine(Convert.ToString(output_filt_signal[hh]));
+
+                        }
+
+                    }
+
                     if (stability_check)
                     {
+
 
                         Console.Write("The filter is stable");
                         Console.Write("\n");
@@ -116,6 +189,20 @@ namespace IIR_Butterworth_C_Sharp
 
                     stability_check = IBI.Check_stability_iir(coeff_final);
 
+                    output_filt_signal = IBI.Filter_Data(coeff_final, test_signal);
+
+                    using (StreamWriter sw = new StreamWriter("Filtered_data.txt"))
+                    {
+
+                        for (int hh = 0; hh < length_test_signal; hh++)
+                        {
+
+                            sw.WriteLine(Convert.ToString(output_filt_signal[hh]));
+
+                        }
+
+                    }
+
                     if (stability_check)
                     {
 
@@ -174,9 +261,23 @@ namespace IIR_Butterworth_C_Sharp
                     }
 
 
-                    coeff_final = IBI.Lp2hp(f2 / Nyquist_F, order_filt);
+                    coeff_final = IBI.Lp2hp(f1 / Nyquist_F, order_filt);
 
                     stability_check = IBI.Check_stability_iir(coeff_final);
+
+                    output_filt_signal = IBI.Filter_Data(coeff_final, test_signal);
+
+                    using (StreamWriter sw = new StreamWriter("Filtered_data.txt"))
+                    {
+
+                        for (int hh = 0; hh < length_test_signal; hh++)
+                        {
+
+                            sw.WriteLine(Convert.ToString(output_filt_signal[hh]));
+
+                        }
+
+                    }
 
                     if (stability_check)
                     {
@@ -236,9 +337,23 @@ namespace IIR_Butterworth_C_Sharp
                     }
 
 
-                    coeff_final = IBI.Lp2lp(f1 / Nyquist_F, order_filt);
+                    coeff_final = IBI.Lp2lp(f2 / Nyquist_F, order_filt);
 
                     stability_check = IBI.Check_stability_iir(coeff_final);
+
+                    output_filt_signal = IBI.Filter_Data(coeff_final, test_signal);
+
+                    using (StreamWriter sw = new StreamWriter("Filtered_data.txt"))
+                    {
+
+                        for (int hh = 0; hh < length_test_signal; hh++)
+                        {
+
+                            sw.WriteLine(Convert.ToString(output_filt_signal[hh]));
+
+                        }
+
+                    }
 
                     if (stability_check)
                     {
